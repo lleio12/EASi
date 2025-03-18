65defd69629f2105b64161ce886506efe3ea2a5e
lleio12 <lucianaleio71@gmail.com>
2025-03-18 17:10:34 +0000

    Ele BLINKA :D

--- source/include/tasks/blynk.h
+++ source/include/tasks/blynk.h
@@ -4,4 +4,4 @@
 
 extern int bomba1;
 
-void iniciarBlynk(UBaseType_t core = 0, uint32_t stackDepth = 2069, UBaseType_t priority = 1);
+void iniciarBlynk(UBaseType_t core = 0, uint32_t stackDepth = 7069, UBaseType_t priority = 1);
