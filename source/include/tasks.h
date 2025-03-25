e510d295efff786fdbf73f4fe8871c0a6c2b5a12
lleio12 <lucianaleio71@gmail.com>
2025-03-25 10:29:55 +0000

    Remove bombas task implementation and references from the project

--- source/include/tasks.h
+++ source/include/tasks.h
@@ -1,13 +1,6 @@
 // Include all tasks here
-
-// bombas, que basicamente é a task que atraca os relés
-#include "tasks/bombas.h"
-
-// display oled 0.91"
 #include "tasks/display.h"
-
 #include "tasks/lightSensor.h"
 #include "tasks/blynk.h"
-
 #include "tasks/buzzer.h"
 #include "tasks/modeControl.h"
